24320b5a4d37d9c0d666e97f40f1cc1c53ffef22
Dillon Hess <dhess268@gmail.com>
2022-04-25 21:52:00 -0400

    add solution

--- solutions.js
+++ solutions.js
@@ -3,3 +3,8 @@ function find_average(array) {
     return array.length ? (array.reduce((a, b) => a + b))/array.length : 0
     }
   
+
+
+    function parseF(s) {
+        return parseFloat(s) ? parseFloat(s) : parseFloat(s) === 0 ? parseFloat(s) : null
+      }
