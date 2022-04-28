7662347c321c2c4aa058b51b78a21e60289957bd
Dillon Hess <dhess268@gmail.com>
2022-04-27 21:26:01 -0400

    add solutions for 4-27

--- solutions.js
+++ solutions.js
@@ -1,10 +1,22 @@
 // finds average of an array
 function find_average(array) {
-    return array.length ? (array.reduce((a, b) => a + b))/array.length : 0
-    }
-  
+  return array.length ? (array.reduce((a, b) => a + b))/array.length : 0
+}
 
 
-    function parseF(s) {
-        return parseFloat(s) ? parseFloat(s) : parseFloat(s) === 0 ? parseFloat(s) : null
-      }
+
+function parseF(s) {
+  return parseFloat(s) ? parseFloat(s) : parseFloat(s) === 0 ? parseFloat(s) : null
+}
+
+
+
+
+// 4-27-22-code but I dont want to create a new folder
+
+
+
+function mango(quantity, price){
+  console.log(quantity/3)
+  return (quantity * price) - (Math.floor(quantity / 3) * price)
+}
