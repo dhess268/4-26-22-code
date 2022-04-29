3d5187b8fc396a6deffa7343e07b28a1856f5781
Dillon Hess <dhess268@gmail.com>
2022-04-28 22:43:12 -0400

    add solution

--- solutions.js
+++ solutions.js
@@ -20,3 +20,38 @@ function mango(quantity, price){
   console.log(quantity/3)
   return (quantity * price) - (Math.floor(quantity / 3) * price)
 }
+
+
+function isVow(a){
+  let vowels = {
+    'a': 97,
+    'e': 101,
+    'i': 105,
+    'o': 111,
+    'u': 117
+  }
+  let newArr = []
+  for(let i = 0; i < a.length; i++){
+    if(a[i] === vowels['a']){
+      newArr.push('a')
+    }
+    else if(a[i] === vowels['i']){
+      newArr.push('i')
+    }
+    else if(a[i] === vowels['e']){
+      newArr.push('e')
+    }
+    else if(a[i] === vowels['o']){
+      newArr.push('o')
+    }
+    else if(a[i] === vowels['u']){
+      newArr.push('u')
+    }
+    else{
+      newArr.push(a[i])
+    }
+    
+      
+    }
+   return newArr
+}
