9daf789b91fe8dda9f3be6f1da65e4db361f5683
Dillon Hess <dhess268@gmail.com>
2022-05-10 22:15:22 -0400

    add solution

--- solutions.js
+++ solutions.js
@@ -143,3 +143,38 @@ function howMuchILoveYou(nbPetals) {
   
   return sayings[(nbPetals - 1) % 6]
 }
+
+
+function switchItUp(number){
+switch (number){
+    case 0:
+    return "Zero"
+    break;
+    case 1:
+    return "One";
+    break;
+    case 2: 
+    return "Two";
+    break;
+    case 3:
+    return "Three"
+    break;
+    case 4:
+    return "Four";
+    break;
+    case 5:
+    return "Five";
+    case 6:
+    return "Six";
+    case 7:
+    return "Seven";
+    break;
+    case 8:
+    return "Eight"
+    break;
+    case 9:
+    return "Nine";
+    break;
+    
+}
+}
