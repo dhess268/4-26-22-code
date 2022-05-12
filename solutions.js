252e2c1c2d2f01a59a8c3a4e9b0cc56859000753
Dillon Hess <dhess268@gmail.com>
2022-05-11 21:57:52 -0400

    add solution

--- solutions.js
+++ solutions.js
@@ -182,3 +182,8 @@ switch (number){
 function apple(x){
   return Math.pow(x, 2) > 1000 ? 'It\'s hotter than the sun!!' : 'Help yourself to a honeycomb Yorkie for the glovebox.' ;
 }
+
+
+function litres(time) {
+  return Math.floor(time * .5)
+}
