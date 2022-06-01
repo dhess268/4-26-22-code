2956ff1100af8a5673c379126e0d03d423620ae2
Dillon Hess <dhess268@gmail.com>
2022-05-31 21:54:21 -0400

    add solution

--- solutions.js
+++ solutions.js
@@ -356,3 +356,20 @@ function checkExam(array1, array2) {
   let score = pointArray.reduce((prev, curr) => {return prev + curr}, 0)
   return score > 0 ? score : 0
  }
+
+ var countDeafRats = function(town) {
+  let deafs = 0
+  let ident = 'O'
+  for ( let i = 0; i < town.length; i++){
+     if ( town[i] === 'P' ){
+      ident =  '~'
+     }
+     if (town[i] === ident){
+       deafs++
+     }
+     if (town[i] === '~' || town[i] === 'O'){
+      i++
+     }
+   }
+  return deafs;
+}
