5eea78a7c7d64b35fd8d8ea6e4c01f0e705d59a2
Dillon Hess <dhess268@gmail.com>
2022-06-01 21:53:53 -0400

    add solution

--- solutions.js
+++ solutions.js
@@ -373,3 +373,13 @@ function checkExam(array1, array2) {
    }
   return deafs;
 }
+
+function towerBuilder(nFloors) {
+  var tower = [];
+  for (var i = 0; i < nFloors; i++) {
+    tower.push(" ".repeat(nFloors - i - 1)
+             + "*".repeat((i * 2)+ 1)
+             + " ".repeat(nFloors - i - 1));
+  }
+  return tower;
+}
