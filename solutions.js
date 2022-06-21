697539e328c36585462632cdd241c4e27ef80f37
Dillon Hess <dhess268@gmail.com>
2022-06-20 22:16:46 -0400

    add solution

--- solutions.js
+++ solutions.js
@@ -493,3 +493,16 @@ function chain(input, fs) {
       return 0
     }
   }
+
+
+  function dirReduc(plan) {
+    var opposite = {
+      'NORTH': 'SOUTH', 'EAST': 'WEST', 'SOUTH': 'NORTH', 'WEST': 'EAST'};
+    return plan.reduce(function(dirs, dir){
+        if (dirs[dirs.length - 1] === opposite[dir])
+          dirs.pop();
+        else
+          dirs.push(dir);
+        return dirs;
+      }, []);
+  }
